cd5aec0a59decaca8aeedb6f8f527334d2be632c
Yash <139779840+yashop7@users.noreply.github.com>
2026-03-02 04:19:09 +0530

    fix: update error message for invalid order quantity in MarketOrder instruction + ReadMe

--- programs/stanx/src/instructions/marketorder.rs
+++ programs/stanx/src/instructions/marketorder.rs
@@ -129,7 +129,7 @@ impl<'info> MarketOrder<'info> {
 
         require!(
             order_amount > 0,
-            PredictionMarketError::InvalidOrderQuantity
+            PredictionMarketError::InvalidAmount
         );
 
         let user_stats: &mut Box<Account<'_, UserStats>> = &mut self.user_stats_account;
